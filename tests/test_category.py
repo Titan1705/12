d383d916b64ac8260f4c7fd809375955a710ac83
Stanley Ipkis <ruben_2015@mail.ru>
2024-03-19 07:48:33 +0300

    add over

---
+++ tests/test_category.py
@@ -0,0 +1,15 @@
+import pytest
+from src.category import Category
+
+
+@pytest.fixture()
+def Category_Phone():
+    return Category('nokia', 'plastic', '6230i')
+
+
+def test_init(Category_Phone):
+    assert Category_Phone.name == 'nokia'
+    assert Category_Phone.description == 'plastic'
+    assert Category_Phone.product == '6230i'
+    assert Category_Phone.number_of_categories == 1
+    assert Category_Phone.unique_product == 1
