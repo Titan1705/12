19aceb999570ccc4be22ca3aaf6248d3343cb232
Stanley Ipkis <ruben_2015@mail.ru>
2024-03-22 18:12:31 +0300

    new add1,3

--- tests/test_product.py
+++ tests/test_product.py
@@ -12,3 +12,12 @@ def test_init2(Product_Samsung):
     assert Product_Samsung.description == '65'
     assert Product_Samsung.price == '45000'
     assert Product_Samsung.availability == 1
+
+
+def test_init_new_product(dict_with_product, for_category):
+    assert len(for_category.products) == 2
+    assert len(Product.products_list) == 2
+    Product.init_new_product(dict_with_product, for_category)
+    assert len(for_category.products) == 3
+    Product.init_new_product(dict_with_product, for_category)
+    assert len(for_category.products) == 3
